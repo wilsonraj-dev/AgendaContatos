9b1843ea60cd0c3888978074b2e73b05ddadc16c
Wilson Junior <58340661+wilsonraj-dev@users.noreply.github.com>
2023-08-08 22:06:11 -0300

    Criação e configuração da classe de contexto

---
+++ AgendaContatos/AgendaContatos/Context/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using AgendaContatos.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AgendaContatos.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> op) : base(op) { }
+
+        public DbSet<Contato> Contatos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+            builder.Entity<Contato>().HasKey(x => x.Id);
+            builder.Entity<Contato>().Property(x => x.Nome).HasMaxLength(100).IsRequired();
+            builder.Entity<Contato>().Property(x => x.Sobrenome).HasMaxLength(100);
+            builder.Entity<Contato>().Property(x => x.Celular).IsRequired();
+            builder.Entity<Contato>().Property(x => x.Favorito).HasMaxLength(1).IsRequired();
+        }
+    }
+}
