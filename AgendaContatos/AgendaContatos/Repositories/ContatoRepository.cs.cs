bd170af9185fed8346a864c35afc0f95d41b085b
Wilson Junior <58340661+wilsonraj-dev@users.noreply.github.com>
2023-08-09 21:54:15 -0300

    Criação e implementação dos repositórios

---
+++ AgendaContatos/AgendaContatos/Repositories/ContatoRepository.cs.cs
@@ -0,0 +1,50 @@
+﻿using AgendaContatos.Context;
+using AgendaContatos.Entities;
+using AgendaContatos.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace AgendaContatos.Repositories
+{
+    public class ContatoRepository : IContatoRepository
+    {
+        private readonly AppDbContext _context;
+
+        public ContatoRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Contato>> GetAll()
+        {
+            return await _context.Contatos.AsNoTracking().ToListAsync();
+        }
+
+        public async Task<IEnumerable<Contato>> GetContatosFavoritos()
+        {
+            return await _context.Contatos.AsNoTracking().Where(x => x.Favorito == 'Y').ToListAsync();
+        }
+
+        public async Task<IEnumerable<Contato>> GetContatosPeloNome(string nome)
+        {
+            return await _context.Contatos.AsNoTracking().Where(x => x.Nome.Contains(nome)).ToListAsync();
+        }
+
+        public async Task AddAsync(Contato contato)
+        {
+            _context.Contatos.Add(contato);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(Contato contato)
+        {
+            _context.Contatos.Update(contato);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task RemoveAsync(int? id)
+        {
+            _context.Contatos.Remove(await _context.Contatos.FindAsync(id) ?? throw new ArgumentException());
+            await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ AgendaContatos/AgendaContatos/Repositories/Interfaces/IContatoRepository.cs
@@ -0,0 +1,14 @@
+﻿using AgendaContatos.Entities;
+
+namespace AgendaContatos.Repositories.Interfaces
+{
+    public interface IContatoRepository
+    {
+        Task<IEnumerable<Contato>> GetAll();
+        Task<IEnumerable<Contato>> GetContatosPeloNome(string nome);
+        Task<IEnumerable<Contato>> GetContatosFavoritos();
+        Task AddAsync(Contato contato);
+        Task UpdateAsync(Contato contato);
+        Task RemoveAsync(int? id);
+    }
+}
